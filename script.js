4bcb740a22161fdfae20435da7a8a100e7b7fef2
Arfa K A <147141631+ArfaKA-03@users.noreply.github.com>
2025-08-09 20:26:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick = () => {
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const section = document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if (top > offset && top < offset + height) {
+            sec.classList.add('start-animation');
+            navlink.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+}
+document.getElementById("resume-btn").addEventListener("click", function () {
+    const link = document.createElement("a");
+    link.href = "Resume.pdf"; // path to your PDF
+    link.download = "Resume.pdf"; // download file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+//contact form- alert saying form submitted
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // stop default form redirect
+    const form = e.target;
+
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+        headers: { 'Accept': 'application/json' }
+    }).then(response => {
+        if (response.ok) {
+            form.reset();
+            alert("Your message has been sent!");
+        } else {
+            alert("Something went wrong. Please try again.");
+        }
+    });
+});
